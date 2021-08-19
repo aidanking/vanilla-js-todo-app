816fa23a6fa5f574c541b740431c0743a6d0769d
Aidan King <aidan.king151@gmail.com>
2021-08-19 21:04:40 +0100

    add delete

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 (() => {
   const addTodoForm = document.querySelector('.add-todo-form');
   const addTodoInput = document.querySelector('.add-todo-input');
-  const todosElement = document.querySelector('.todos');
-  const todos = [];
+  let todos = [];
+  let maxId = 0;
 
   function addTodo(e) {
     e.preventDefault();
+    maxId++;
 
     const todo = {
+      id: maxId,
       name: addTodoInput.value,
     };
 
@@ -18,10 +20,35 @@
   function addTodoElement(todo) {
     const todosElement = document.querySelector('.todos');
     const todoElement = document.createElement('li');
+
     todoElement.innerText = todo.name;
+    todoElement.append(createDeleteButton());
+    todoElement.className = 'todo';
+    todoElement.dataset.todoId = todo.id;
 
     todosElement.append(todoElement);
-    addTodoInput.value = '';
+    addTodoForm.reset();
+  }
+
+  function createDeleteButton() {
+    const deleteButton = document.createElement('button');
+
+    deleteButton.innerText = 'Delete Todo';
+
+    deleteButton.addEventListener('click', deleteTodo);
+
+    return deleteButton;
+  }
+
+  function deleteTodo(e) {
+    const { target } = e;
+
+    const todoElementToBeDeleted = target.parentElement;
+    const { todoId } = todoElementToBeDeleted.dataset;
+
+    todos = todos.filter((x) => x.id !== Number(todoId));
+
+    todoElementToBeDeleted.remove();
   }
 
   addTodoForm.addEventListener('submit', addTodo);
